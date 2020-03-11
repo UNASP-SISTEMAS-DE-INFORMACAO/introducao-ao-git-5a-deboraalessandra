66aac41dc23edd4c696a8bac7d1c0c89d74d5637
deboraalessandra <deboraalessandraxd@gmail.com>
2020-03-10 22:37:06 -0300

    desafio em sala pra ganhar presença

---
+++ desafio.py
@@ -0,0 +1,30 @@
+#exemplo if
+idade = 17
+if idade > 18:
+    print("Você é maior de idade")
+else:
+    print("Você é menor de idade")
+
+#exemplo while
+    x = 0
+while x < 10:
+    print(x)
+    x += 1
+    if x == 6:
+        print("x é igual a 6")
+        break
+    else:
+    print("fim while")
+
+
+#exemplo função
+
+def hello (meu_nome):
+    print ('Olá', meu_nome)
+
+#exemplo for
+nomes = ['Débora', 'Talyta', 'Leticia']
+for n in nomes:
+     print(n)
+else:
+     print("Todos os nomes foram listados com sucesso")
---
+++ primeiroPrograma.py
@@ -0,0 +1,13 @@
+print ("Olá")
+n1 = 2
+n2 = 3
+
+soma = n1 + n2
+
+if (soma<6):
+    print("Maior que 6")
+
+    for i in range(10):
+    print(i) 
+
+print(soma)
